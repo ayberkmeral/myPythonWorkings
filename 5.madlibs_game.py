976c7773387677acbe5ba95d1aecd7d5b2d3a032
ayberkmeral <meraldursunayberk@gmail.com>
2025-03-03 12:22:00 +0300

    Add files via upload
    Python Madlibs game

---
+++ 5.madlibs_game.py
@@ -0,0 +1,9 @@
+adejective1=input("enter an adjactive: ")
+noun=input("enter a noun: ")
+verb=input("enter a verb: ")
+adejective2=input("enter an adjactive: ")
+adejective3=input("enter an adjactive: ")
+
+
+print(f"ı went to a {adejective1} park today")
+print(f"we {verb}ed with {noun}s and it was {adejective3} and also {adejective2}")
